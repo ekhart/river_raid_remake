24b6ca09b30d8eb80d83fc45614562b611112b05
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-01 21:20:28 +0100

    simple fire

--- game.gd
+++ game.gd
@@ -2,14 +2,30 @@ extends Node2D
 
 const SHIP_HORIZONTAL_SPEED = 150
 const SHIP_VERTICAL_SPEED = 150
+const BULLET_SPEED = 300
 
 var ship
+var bullet
 
 func _ready():
 	ship = get_node("ship")
+	bullet = get_node("bullet")
+	bullet.hide()
 	set_process(true)
 
 func _process(delta):
+	ship.set_pos(get_ship_pos(delta))
+	
+	if (Input.is_action_pressed("ui_accept")):
+		bullet.show()
+		
+	if (bullet.is_visible()):
+		var bullet_pos = bullet.get_pos()
+		bullet_pos.y += -BULLET_SPEED * delta
+		bullet.set_pos(bullet_pos)
+		
+	
+func get_ship_pos(delta):
 	var ship_pos = ship.get_pos()
 	
 	if (Input.is_action_pressed("ui_up")):
@@ -23,6 +39,7 @@ func _process(delta):
 
 	if (Input.is_action_pressed("ui_right")):
 		ship_pos.x += SHIP_VERTICAL_SPEED * delta
-		
-	ship.set_pos(ship_pos)
+	
+	return ship_pos
+	
 	

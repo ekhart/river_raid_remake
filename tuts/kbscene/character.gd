53d9e5d87ff8235c4bbd92a2ce58f19e5890c0bf
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-07 14:23:57 +0100

    Moving the Kinematic character: move down 1 pix / second

--- tuts/kbscene/character.gd
+++ tuts/kbscene/character.gd
@@ -1,10 +1,7 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	move(Vector2(0, 1)) # move down 1 pixel per physics frame

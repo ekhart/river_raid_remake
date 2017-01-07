871394ae4a858d755b530c0c825575efb1370f15
Artur Dębkowski <arturdebkowski@gmail.com>
2017-01-07 14:16:36 +0100

    +1 life za > 10k points

--- game.gd
+++ game.gd
@@ -8,6 +8,8 @@ const FUEL_REFUEL_STEP = 1
 const FUEL_ALMOST_MAX = FUEL_MAX - FUEL_REFUEL_STEP
 
 const LIVES_MAX = 3
+const LIVES_STEP = 1
+const LIVES_NEXT = 10000
 
 
 var viewport_size
@@ -30,7 +32,7 @@ var last_bridge
 func _ready():
 	viewport_size = get_viewport().get_rect().size
 
-	score = 0
+	score = 9900
 	score_label = get_node("hud/score")
 	fuel = FUEL_MAX
 	fuel_label = get_node("hud/fuel")
@@ -82,17 +84,26 @@ func set_fuel():
 
 
 func set_score(points):
+	var last_score = score
+	
 	score += points
 	score_label.set_text("SCORE: " + str(score))
+	
+	var last_mod = last_score % LIVES_NEXT
+	var current_mod = score % LIVES_NEXT
+	
+	if last_mod > current_mod:
+		set_lives(LIVES_STEP)
 
 
-func set_lives():
-	lives -= 1
+func set_lives(number = -LIVES_STEP):
+	lives += number
 	lives_label.set_text("LIVES: " + str(lives))
 	
-	set_pos(Vector2(0, last_bridge.get_pos().y + 250))
-	ship.set_global_pos(ship.START_POS) # reset ship position
-	last_bridge.show()
+	if number == -LIVES_STEP:
+		set_pos(Vector2(0, last_bridge.get_pos().y + 250))
+		ship.set_global_pos(ship.START_POS) # reset ship position
+		last_bridge.show()
 
 
 func is_tile_fuel():

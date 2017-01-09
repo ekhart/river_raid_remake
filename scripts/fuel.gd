5df884037b0dc8d39b33b3cae56303155b6a18c8
Artur Dębkowski <arturdebkowski@gmail.com>
2017-01-09 11:06:29 +0100

    progess & hide_bullet, set_score on fuel boom

--- scripts/fuel.gd
+++ scripts/fuel.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 
+const SCORE = 100
+
+
 var game
 
 
@@ -10,8 +13,22 @@ func _ready():
 
 func _on_fuel_area_enter(area):
 	game.is_refuel = area == game.ship
+	
+	if area == game.bullet:
+		destroy()
+		game.bullet.hide_bullet()
+		game.set_score(SCORE)
 
 
 func _on_fuel_area_exit(area):
 	if area == game.ship:
 		game.is_refuel = false
+
+
+func destroy():
+	get_node("sprite").hide()
+	get_node("boom").show()
+	get_node("boom_sfx").play("boom")
+	get_node("boom/animation").play("animation")
+	disconnect("area_enter", self, "_on_fuel_area_enter")
+	disconnect("area_exit", self, "_on_fuel_area_exit")

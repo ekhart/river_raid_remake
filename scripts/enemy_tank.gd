3f92cea55bcd7230d1a1db1591822a6e80d3da46
Artur Dębkowski <arturdebkowski@gmail.com>
2017-01-12 12:56:06 +0100

    clean enemy_tank

--- scripts/enemy_tank.gd
+++ scripts/enemy_tank.gd
@@ -1,5 +1,6 @@
 extends "res://scripts/enemy.gd"
 
+
 const SPEED = 2
 const SCORE_ENEMY = 250
 const CALL_BASE_FIXED_PROCESS = false
@@ -9,11 +10,3 @@ func _fixed_process(delta):
 	var pos = get_pos()
 	pos.x += SPEED
 	set_pos(pos)
-
-
-func set_enemy_scale(pos):
-	pass
-
-
-func _on_enemy_body_enter(body):
-	pass

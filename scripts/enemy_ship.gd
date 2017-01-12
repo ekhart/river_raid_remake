a990234c5b88dda132a15ebe8f618bba4f15bac9
Artur Dębkowski <arturdebkowski@gmail.com>
2017-01-12 21:30:39 +0100

    other moving of enemy_ship

--- scripts/enemy_ship.gd
+++ scripts/enemy_ship.gd
@@ -1,4 +1,12 @@
 extends "res://scripts/enemy.gd"
 
 
+const SPEED = 3
 const SCORE_ENEMY = 100
+const CALL_BASE_FIXED_PROCESS = false
+
+
+func _fixed_process(delta):
+	var pos = get_pos()
+	pos.x += SPEED
+	set_pos(pos)
